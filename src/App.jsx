86302047683d21ed04065211e3f90a52d3c98f22
Anatolii <tolipus@gmail.com>
2025-02-18 20:44:15 +0200

    redux custom

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
-import {useState} from 'react'
 
 import './App.css'
 import Balance from "./components/Balance.jsx";
 import Operation from "./components/Operation.jsx";
 
 function App() {
-    const [balance, setBalance] = useState(1100);
-    const deposit = sum => setBalance(prevBalance => prevBalance + sum);
-    const withdraw = sum => setBalance(prevBalance => prevBalance < sum ? prevBalance : prevBalance - sum);
-
-    return (
+        return (
 
         <div>
-            <Balance balance={balance}/>
-            <Operation deposit={deposit} withdraw={withdraw}/>
+            <Balance/>
+            <Operation/>
         </div>
 
     )
--- src/components/Operation.jsx
+++ src/components/Operation.jsx
@@ -1,18 +1,20 @@
 import {useState} from "react";
+import {store} from "../configureStore/store.js";
+import {deposit, withdraw} from "../actions/accountActions.js";
 
-const Operation = ({deposit, withdraw}) => {
+const Operation = () => {
     const[sum, setSum] = useState(0);
     return (
         <div>
             <button
-                onClick={() => withdraw(sum)}
+                onClick={() => store.dispatch(withdraw(sum))}
             >Withdraw</button>
             <input
                 type={'number'}
                 onChange={e=> setSum(+e.target.value)}
                 value={sum}/>
             <button
-                onClick={() => deposit(sum)}>
+                onClick={() => store.dispatch(deposit(sum))}>
                 Deposit</button>
         </div>
     );
